71f16aafda858bba3242a82a536c8096ee7d58cd
Karl G <karlg100@gmail.com>
2018-07-26 07:17:54 -0400

    get/set attributes

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ This is a quick and dirty IBM WorkLight client to talk to the Frigidaire cloud s
 
 # Done
 * Auth is working!
+* setting attributes values works!
+* getting attribute values works!
+* function library complete for homebridge plugin support!
 
-# TODO
-* figure out how the X-WL-ClientId is generated.  it appears to be a sha1sum of something, not sure what.  (is not OS version)  probably some conbination of the app version and device string
-* build out a way to send commands
-* build out a way to receive telementry and update it
-* build out a function library to hand all the device controls for homebridge support
+# Todo
+* figure out how the X-WL-ClientId is generated.  it appears to be a sha1sum of something, not sure what.  
+** tested with another device, it seems to definately be dependant upon the device type
+** not based on iOS version
+** probably some conbination of the app version and device string
+** needs to match their database of expected device string, or will fail auth chain
+* build out a way to receive updated telementry and cache it locally
+* add a timer to recieve updated telementry (try to keep the timing to the observed behavor in the real client.  seems to be every 2-4 seconds.  however, maybe we need less if homekit is not focued?  this may be done on the homebridge plugin side)
--- demo.js
+++ demo.js
@@ -1,4 +1,5 @@
 var Frigidaire = require('frigidaire');
+var util = require('util');
 
 console.log('starting up');
 var ac = new Frigidaire({
@@ -17,33 +18,43 @@ switch (command) {
   // login, get devices and current telementry
   case 'telem':
   case 'get':
-    ac.getTelem([], function(err, result) {
+    ac.getTelem(function(err, result) {
       if (err) return console.error(err);
       console.log('Got Telem');
       console.log(result);
+      console.log(util.inspect(ac, false, null));
+    });
+    break;
+
+  // Mode
+   case 'getMode':
+    ac.getMode(function(err, result) {
+      if (err) return console.error(err);
       console.log(ac);
     });
     break;
 
-  // mode
   case 'off':
     ac.mode(ac.MODE_OFF, function(err, result) {
       if (err) return console.error(err);
       console.log('Turned off');
     });
     break;
+
   case 'cool':
     ac.mode(ac.MODE_COOL, function(err, result) {
       if (err) return console.error(err);
       console.log('Changed to cool mode');
     });
     break;
+
   case 'econ':
     ac.mode(ac.MODE_ECON, function(err, result) {
       if (err) return console.error(err);
       console.log('Changed to econ mode');
     });
     break;
+
   case 'fan':
     ac.mode(ac.MODE_FAN, function(err, result) {
       if (err) return console.error(err);
@@ -51,7 +62,14 @@ switch (command) {
     });
     break;
 
-  // change units
+  // Units
+   case 'getUnit':
+    ac.getUnit(function(err, result) {
+      if (err) return console.error(err);
+      console.log('Mode is '+result);
+    });
+    break;
+
   case 'f':
   case 'fahrenheit':
     ac.changeUnits(ac.FAHRENHEIT,function(err, result) {
@@ -59,6 +77,7 @@ switch (command) {
       console.log('Changed to fahrenheit');
     });
     break;
+
   case 'c':
   case 'celcius':
     ac.changeUnits(ac.CELCIUS,function(err, result) {
@@ -67,13 +86,21 @@ switch (command) {
     });
     break;
 
-  // clean air
+  // Clean Air
+   case 'getClean':
+    ac.getCleanAir(function(err, result) {
+      if (err) return console.error(err);
+      console.log('Clean air is '+result);
+    });
+    break;
+
   case 'clean':
     ac.cleanAir(ac.CLEANAIR_ON, function(err, result) {
       if (err) return console.error(err);
       console.log('Turned on clean air');
     });
     break;
+
   case 'rec':
     ac.cleanAir(ac.CLEANAIR_OFF, function(err, result) {
       if (err) return console.error(err);
@@ -81,12 +108,20 @@ switch (command) {
     });
     break;
 
-  // fan mode
-  case 'auto':
+  // Fan Mode
+  case 'getFan':
+    ac.getFanMode(function(err, result) {
+      if (err) return console.error(err);
+      console.log('current fan mode is '+result);
+    });
+    break;
+
+ case 'auto':
     ac.fanMode(ac.FANMODE_AUTO, function(err, result) {
       if (err) return console.error(err);
       console.log('Turned fan to auto');
     });
+    break;
 
   case 'high':
     ac.fanMode(ac.FANMODE_HIGH, function(err, result) {
@@ -94,26 +129,44 @@ switch (command) {
       console.log('Turned fan to high');
     });
     break;
+
   case 'med':
     ac.fanMode(ac.FANMODE_MED, function(err, result) {
       if (err) return console.error(err);
       console.log('Turned fan to medium');
     });
     break;
+
   case 'low':
     ac.fanMode(ac.FANMODE_LOW, function(err, result) {
       if (err) return console.error(err);
       console.log('Turned fan to low');
     });
+    break;
 
   case 'temp':
     ac.setTemp(arg, function(err, result) {
       if (err) return console.error(err);
-      console.log('Turned fan to low');
+      console.log('changed temp to '+arg);
     });
+    break;
+
+  case 'getTemp':
+    ac.getTemp(function(err, result) {
+      if (err) return console.error(err);
+      console.log('current temp is '+result);
+    });
+    break;
+
+  case 'getRoomTemp':
+    ac.getRoomTemp(function(err, result) {
+      if (err) return console.error(err);
+      console.log('current room temp is '+result);
+    });
+    break;
 
   default:
     console.error('Unknown command:', command);
-    console.error('Available commands are: get|telem, off, cool, econ, fan, f|fahrenheit, c|celcius, clean, rec, auto, high, med, low');
+    console.error('Available commands are: get|telem, getMode, off, cool, econ, fan, getUnit, f|fahrenheit, c|celcius, getClean, clean, rec, getFan, auto, high, med, low, setTemp, getTemp, getRoomTemp');
     break;
 }
